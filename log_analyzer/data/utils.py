90e78d3565e80b90fd6045419f1e141ca607a6ba
Jakob Nyberg <jaknyb@kth.se>
2021-03-30 15:25:12 +0200

    data loader seems to work

--- log_analyzer/data/utils.py
+++ log_analyzer/data/utils.py
@@ -3,11 +3,91 @@
 Data loading functions
 """
 
-from torch.utils.data import DataLoader
+from typing import Iterator
+from torch.utils.data import DataLoader, IterableDataset
+import torch
+import os.path as path
+import numpy as np
 
-def create_data_loaders():
+DEFAULT_HEADERS = [
+    "line_number",
+    "second",
+    "day",
+    "user",
+    "red",
+    "seq_len",
+    "start_sentence"
+]
 
-    train_loader = DataLoader()
-    test_loader = DataLoader()
+class IterableLogDataSet(IterableDataset):
+
+    def __init__(self, filepath, bidirectional, skipsos, jagged, delimiter=' ') -> None:
+        super().__init__()
+        self.filepath = filepath
+        self.delimiter = delimiter
+
+        self.skipsos = skipsos
+        self.jag = jagged
+        self.bidir = bidirectional
+        self.sentence_length = 120 #TODO have this be set in config file
+
+    def parse_lines(self, filepath):
+        with open(filepath, 'r') as f:
+            for line in f:
+                split_line = line.strip().split(self.delimiter)
+                split_line = [int(x) for x in split_line]
+                data = torch.LongTensor(split_line)
+
+                endx = data.shape[0] - int(not self.bidir)
+                endt = data.shape[0] - int(self.bidir)
+
+                datadict = {
+                    'line':     data[0],
+                    'second':   data[1],
+                    'day':      data[2],
+                    'user':     data[3],
+                    'red':      data[4],
+                    'x':        data[(5+self.jag+self.skipsos):endx],
+                    't':        data[(6+self.jag+self.skipsos):endt]
+                    }
+
+                if self.jag:
+                    datadict['lengths'] = data[5]
+                    datadict['mask'] = get_mask(datadict['lengths']-2*self.bidir-self.skipsos, self.sentence_length-2*self.args.bidir)
+                    assert np.all(datadict['lengths'] <= datadict['x'].shape[1]), 'Sequence found greater than num_tokens_predicted'
+                    assert np.nonzero(datadict['lengths'])[0].shape[0] == datadict['lengths'].shape[0], \
+                        'Sequence lengths must be greater than zero.' \
+                        'Found zero length sequence in datadict["lengths"]: %s' % datadict['lengths']
+
+                yield datadict
+
+    def __iter__(self):
+        return self.parse_lines(self.filepath)
+
+
+def create_data_loader(filepath, args):
+    ds = IterableLogDataSet(filepath, args.bidirectional, args.skipsos, args.jagged)
+    return DataLoader(ds, batch_size=args.batch_size)
+
+def load_data(day_train, day_eval, args):
+
+    filepath_train = path.join(args.data_folder, f"{day_train}.txt")
+    filepath_eval = path.join(args.data_folder, f"{day_eval}.txt")
+    train_loader = create_data_loader(filepath_train, args)
+    test_loader = create_data_loader(filepath_eval, args)
 
     return train_loader, test_loader
+
+def get_mask(lens, num_tokens):
+    """
+    For masking output of lm_rnn for jagged sequences for correct gradient update.
+    Sequence length of 0 will output nan for that row of mask so don't do this.
+
+    :param lens: Numpy vector of sequence lengths
+    :param num_tokens: (int) Number of predicted tokens in sentence.
+    :return: A numpy array mask MB X num_tokens
+             For each row there are: lens[i] values of 1/lens[i]
+                                     followed by num_tokens - lens[i] zeros
+    """
+    mask_template = torch.arange(num_tokens, dtype=torch.float)
+    return (mask_template < lens) / lens
--- train_model.py
+++ train_model.py
@@ -1,17 +1,34 @@
 from argparse import ArgumentParser
 import argparse
-
-import log_analyzer.data.utils
+import numpy as np
+import log_analyzer.data.utils as data_utils
 
 """
 Entrypoint script for training
 """
 
-def train():
+def train(args):
+    train_loader, test_loader = data_utils.load_data(0, 1, args)
 
+    jag = int(args.jagged)
+    skipsos = int(args.skipsos)
+
+    for batch in train_loader:
+
+        pass
+        
     pass
 
 if __name__ == "__main__":
-    args = ArgumentParser()
-    args.add_argument("data-folder", type=str, help="Path to data files.")
+    parser = ArgumentParser()
+    parser.add_argument("--data-folder", type=str, help="Path to data files.")
+    parser.add_argument('--jagged', action='store_true',
+                        help='Whether using sequences of variable length (Input should'
+                             'be zero-padded to max_sequence_length.')
+    parser.add_argument('--skipsos', action='store_true',
+                        help='Whether to skip a start of sentence token.')
+    parser.add_argument('--bidir', dest='bidirectional', action='store_true',
+                        help='Whether to use bidirectional lstm for lower tier.')
+    parser.add_argument('-bs', dest='batch_size', type=int, help='batch size.')
+    args = parser.parse_args()
     train(args)
