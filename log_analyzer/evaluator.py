2d74192599b5e96f4d60ebb6388af1f2a5e8abd5
Simon Gökstorp <gokstorp@kth.se>
2021-05-19 15:23:53 +0200

    Implement token accuracy metric

--- log_analyzer/evaluator.py
+++ log_analyzer/evaluator.py
@@ -45,6 +45,21 @@ class Evaluator:
 
     def get_token_accuracy(self):
         """Computes the accuracy of the model token prediction"""
+        # Flatten the log_line and predictions lists
+        flattened_lines = np.array([])
+        for line in self.data["log_lines"]:
+            flattened_lines = np.concatenate((flattened_lines, line.numpy()))
+        flattened_preds = np.array([])
+        for pred in self.data["predictions"]:
+            flattened_preds = np.concatenate((flattened_preds, pred.numpy()))
+
+        # Subtract each predicted token from the actual token
+        # Thus a value of 0 in matches indicates a correct prediction
+        matches = flattened_lines - flattened_preds
+
+        # % accuracy = 1 - number_of_non_matches/number_of_tokens
+        accuracy = 1 - (np.count_nonzero(matches) / len(matches))
+        return accuracy
 
     def get_token_perplexity(self):
         """Computes and returns the perplexity of the model token prediction"""
