546aa26f2c1430571d6b2c97ffa17503692d85ee
Jakob Nyberg <jaknyb@kth.se>
2021-05-24 13:48:47 +0200

    add data config as subclass

--- log_analyzer/config/trainer_config.py
+++ log_analyzer/config/trainer_config.py
@@ -1,13 +1,24 @@
-
-from argparse import Namespace
 from log_analyzer.config.config import Config
 
 
+class DataConfig(Config):
+
+    def __init__(self, train_files, test_files, sentence_length, vocab_size, number_of_days) -> None:
+        super().__init__()
+        self.train_files = train_files
+        self.test_file = test_files
+        self.sentence_length = sentence_length
+        self.vocab_size = vocab_size
+        self.number_of_days = number_of_days
+
 class TrainerConfig(Config):
 
-    def __init__(self, batch_size, jagged, bidirectional, tiered, learning_rate, early_stopping, 
+    def __init__(self, data_config : dict, batch_size, jagged, bidirectional, tiered, learning_rate, early_stopping, 
     early_stop_patience, scheduler_gamma, scheduler_step_size) -> None:
         super().__init__()
+
+        # Data settings
+        self.data_config = data_config
 
         # Model settings
         self.tiered = tiered
@@ -17,7 +28,7 @@ class TrainerConfig(Config):
         self.batch_size = batch_size
         self.learning_rate = learning_rate
         self.early_stopping = early_stopping
-        self.es_patience = early_stop_patience
+        self.early_stop_patience = early_stop_patience
         self.scheduler_gamma = scheduler_gamma
         self.scheduler_step_size = scheduler_step_size
         self.jagged = jagged
