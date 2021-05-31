a0be5a85be253a97f108bd9bc5ea91d258b24940
Simon Gokstorp <gokstorp@kth.se>
2021-05-31 08:32:01 +0000

    Optimise plot_losses_by_line

--- log_analyzer/evaluator.py
+++ log_analyzer/evaluator.py
@@ -92,68 +92,33 @@ class Evaluator:
         """Computes and plots the given (default 75/95/99) percentiles of anomaly score
         (loss) by line for each second"""
         # TODO: support for data spanning more than one day (the data["days"] entry is currently ignored)
-        if (
-            not caching
-            or self.plot_losses_by_line_cache is None
-            or self.plot_losses_by_line_cache[2] != percentiles
-        ):
             smoothing = min(
                 max(1, int(smoothing)), len(self.data["losses"])
             )  # Ensure smoothing is an int and in the range [1, len(losses)]
             if not smoothing % 2:
                 smoothing += 1  # Ensure smoothing is odd
 
-            losses_by_second = []
-            seconds = []
-
+        plotting_data = [[] for _ in percentiles]
             # Create a list of losses for each second
-            for second, loss in tqdm(zip(self.data["seconds"], self.data["losses"])):
-                if second not in seconds:
-                    losses_by_second.append(np.array([loss]))
-                    seconds.append(second)
-                else:
-                    idx = seconds.index(second)
-                    losses_by_second[idx] = np.concatenate(
-                        (losses_by_second[idx], [loss])
-                    )
-            if caching:
-                self.plot_losses_by_line_cache = (
-                    seconds,
-                    losses_by_second,
-                    percentiles,
-                )
+        seconds = np.unique(self.data["seconds"])
+        for second in tqdm(seconds):
+            losses = self.data["losses"][self.data["seconds"] == second]
+            for idx, p in enumerate(percentiles):
+                percentile_data = np.percentile(losses, p)
+                plotting_data[idx].append(percentile_data)
 
-        if caching:
-            seconds, losses_by_second, percentiles = self.plot_losses_by_line_cache
-
-        plotting_data = []
-        for p in percentiles:
-            # iterate over losses_by_second (which contains the list of losses at each second)
-            # and apply np.percentile to compute the "p"th percentile (e.g. 75th percentile)
-            # loss at that second, then append this list of "p"th percentiles to plotting_data
-            percentile_data = map(lambda x: np.percentile(x, p), losses_by_second)
-            # map returns a generator, so apply list() to generate the list
-            percentile_data = list(percentile_data)
             # apply the desired smoothing
+        for idx, _ in enumerate(plotting_data):
             smoothed_data = (
-                np.convolve(percentile_data, np.ones(smoothing), "valid") / smoothing
+                np.convolve(plotting_data[idx], np.ones(smoothing), "same") / smoothing
             )
-            # To avoid boundary effects, 'valid' is used in the convolution (thus producing a list
-            # that is shorter than the input), and the first and last values are re-inserted
-            # to restore the original length
-            for _ in range(int((smoothing - 1) / 2)):
-                smoothed_data = np.insert(smoothed_data, 0, percentile_data[0])
-                smoothed_data = np.insert(
-                    smoothed_data, len(smoothed_data), percentile_data[-1]
-                )
-            plotting_data.append(smoothed_data)
+            plotting_data[idx] = smoothed_data
 
         # Extract all red team events
         red_seconds = self.data["seconds"][self.data["red_flags"] != 0]
         red_losses = self.data["losses"][self.data["red_flags"] != 0]
 
-        # Extract the top X outlier non-red team events
-        # X is 1 per minute
+        # Extract the top X (1 per minute of data) outlier non-red team events
         outlier_count = len(seconds) // 60
         blue_losses = self.data["losses"][self.data["red_flags"] == 0]
         blue_seconds = self.data["seconds"][self.data["red_flags"] == 0]
@@ -161,20 +126,19 @@ class Evaluator:
         outlier_indices = np.argpartition(-blue_losses, outlier_count)[:outlier_count]
         blue_losses = blue_losses[outlier_indices]
         blue_seconds = blue_seconds[outlier_indices]
-        print(len(blue_losses))
 
         # plot the percentile ranges
         for idx in range(len(plotting_data) - 1):
             plt.fill_between(
                 seconds, plotting_data[idx], plotting_data[idx + 1], color=colors[idx]
             )
-        # plot the redteam events
-        plt.plot(red_seconds, red_losses, "r+", label="Red team events")
         # plot the non-redteam outliers
         plt.plot(blue_seconds, blue_losses, "bo", label="Outlier normal events")
+        # plot the redteam events
+        plt.plot(red_seconds, red_losses, "r+", label="Red team events")
 
-        plt.xlabel("x - Time (seconds)")
-        plt.ylabel(f"y - Percentiles of loss {tuple(percentiles)}")
+        plt.xlabel("Time (seconds)")
+        plt.ylabel(f"Percentiles of loss {tuple(percentiles)}")
         plt.title("Aggregate line losses by time")
 
     def plot_ROC_curve(self):
@@ -189,10 +153,10 @@ class Evaluator:
             tp_rate,
             color="orange",
             lw=2,
-            label=f"ROC curve (area = {auc_score}",
+            label=f"ROC curve (area = {auc_score})",
         )
         plt.plot([0, 1], [0, 1], lw=2, linestyle="--")
-        plt.xlabel("x - False Positive Rate")
-        plt.ylabel("y - True Positive Rate")
+        plt.xlabel("False Positive Rate")
+        plt.ylabel("True Positive Rate")
         plt.title("Receiver Operating Characteristic curve")
         return auc_score
