a25b9fd620616a25adc598e921915ecc85e1647a
Yeongwoo Kim <yeongwoo@kth.se>
2021-06-07 18:02:20 +0200

    Specify a class to use

--- parse_log.py
+++ parse_log.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     
     args = arg_parser()
     weekend_days = [3, 4, 10, 11, 17, 18, 24, 25, 31, 32, 38, 39, 45, 46, 47, 52, 53]
-    if args.char_lv:
+    if args.type == 'char_level':
         tokenizer = Char_tokenizer(args, weekend_days)
     else:
         tokenizer = Word_tokenizer(args, weekend_days)
