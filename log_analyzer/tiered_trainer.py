179e0fa1e8037ab7ee26a265d4a56b80b98dfdbd
Jakob Nyberg <jaknyb@kth.se>
2021-06-10 16:33:44 +0200

    Revert "fix to handle batch_size=1"
    This reverts commit 7689d9238c78eeaa08ae969302dafd07f2784b44.

--- log_analyzer/tiered_trainer.py
+++ log_analyzer/tiered_trainer.py
@@ -29,10 +29,7 @@ class TieredTrainer(Trainer):
                 skip_len = 2 if self.bidirectional else 0
                 token_losses = self.criterion(
                     step_output.transpose(1, 2), true_y[:, :max(lengths)-skip_len])
-                try:
-                    masked_losses = token_losses * mask[i, :max(lengths-skip_len)]
-                except IndexError: # This seems to happen when the batch size is 1:
-                    masked_losses = token_losses * mask[:max(lengths-skip_len)]
+                masked_losses = token_losses * mask[i][:max(lengths-skip_len)]
                 line_losses = torch.sum(masked_losses, dim=1)
             else:
                 token_losses = self.criterion(
