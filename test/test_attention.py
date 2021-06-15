e9c32e958a6663dfe6d94306f323038f0045de07
Jakob Nyberg <jaknyb@kth.se>
2021-06-15 15:31:05 +0200

    tests for semantic attention

--- test/test_attention.py
+++ test/test_attention.py
@@ -56,6 +56,30 @@ def test_forward_word_syntax_attention(tmpdir):
 
     utils.run_test(args)
     assert True
+def test_forward_char_semantic_attention(tmpdir):
+    bidir = False
+    model_type = 'lstm'
+    token_level = 'char'
+
+    args = utils.set_args(bidir, model_type, token_level)
+    args['model_config'].attention_type = 'semantic'
+    args['model_config'].attention_dim = 10
+    args['base_logdir'] = os.path.join(tmpdir, 'runs')
+    utils.run_test(args)
+    assert True
+
+def test_forward_word_semantic_attention(tmpdir):
+    bidir = False
+    model_type = 'lstm'
+    token_level = 'word'
+
+    args = utils.set_args(bidir, model_type, token_level)
+    args['model_config'].attention_type = 'semantic'
+    args['model_config'].attention_dim = 10
+    args['base_logdir'] = os.path.join(tmpdir, 'runs')
+
+    utils.run_test(args)
+    assert True
 
 def test_bidirectional_word_attention(tmpdir):
     bidir = True
