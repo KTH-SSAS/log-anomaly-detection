4a354093be0f1b717e2656d9811cbb45fb482681
Yeongwoo Kim <yeongwoo@kth.se>
2021-07-05 16:24:23 +0200

    small example.

--- log_analyzer/tokenizer/detokenizer.py
+++ log_analyzer/tokenizer/detokenizer.py
@@ -28,3 +28,14 @@ class Int2Word(Detokenizer):
         restored_txt = [self.search_dict[pred_token] for pred_token in sentence if pred_token not in self.skip_list]
         return restored_txt
 
+if __name__ == "__main__":
+
+    dict_file = 'parsed_data/word_token/records/word_token_map.json'
+
+    detokenizer = Int2Char()
+    ex_prediction = "0 55 19 18 19 34 38 49 47 19 14 37 19 26 24 20 6 34 38 49 47 19 14 37 19 26 24 20 14 37 19 26 24 20 14 33 14 33 14 35 87 86 74 47 67 82 14 53 87 69 69 71 85 85 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0".split(' ')
+    print(detokenizer.run_detokenizer(ex_prediction))
+
+    detokenizer = Int2Word(dict_file)
+    ex_prediction = "0 5 6 7 6 7 7 8 9 10 11 1".split(' ')
+    print(detokenizer.run_detokenizer(ex_prediction))
