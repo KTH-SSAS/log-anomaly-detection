c8d6a2739d27875acbbe5599253851785d19005c
Yeongwoo Kim <yeongwoo@kth.se>
2021-07-05 16:22:14 +0200

    run the detokenizer converting ints to chars.

--- log_analyzer/tokenizer/detokenizer.py
+++ log_analyzer/tokenizer/detokenizer.py
@@ -8,6 +8,11 @@ class Int2Char(Detokenizer):
     def __init__(self):
         super().__init__()
 
+    def run_detokenizer(self, sentence):
+        restored_lst = [chr(int(pred_token) + 30) for pred_token in sentence if pred_token not in self.skip_list]
+        restored_txt = "".join(restored_lst)
+        return restored_txt.split(',')
+
 class Int2Word(Detokenizer):
     def __init__(self, dict_file):
         super().__init__()
