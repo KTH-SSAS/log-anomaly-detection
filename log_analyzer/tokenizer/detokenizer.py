f1fe89fb99c201e90d820e31ebf7550ad07c2725
Yeongwoo Kim <yeongwoo@kth.se>
2021-07-21 10:32:12 +0200

    read json files

--- log_analyzer/tokenizer/detokenizer.py
+++ log_analyzer/tokenizer/detokenizer.py
@@ -14,10 +14,33 @@ class Int2Char(Detokenizer):
         return restored_txt
 
 class Int2Word(Detokenizer):	
-    def __init__(self, dict_file):	
+    def __init__(self, json_folder, dict_file):	
         super().__init__()	
-        f = open(dict_file)	
-        self.search_dict = json.load(f)	
+
+        with open(os.path.join(json_folder, dict_file)) as json_file:
+            self.search_dict = json.load(json_file)
+        with open(os.path.join(json_folder, 'usr_map.json')) as json_file:
+            self.usr_inds = json.load(json_file)
+        with open(os.path.join(json_folder, 'pc_map.json')) as json_file:
+            self.pc_inds = json.load(json_file)
+        with open(os.path.join(json_folder, 'domain_map.json')) as json_file:
+            self.domain_inds = json.load(json_file)
+        with open(os.path.join(json_folder, 'auth_map.json')) as json_file:
+            self.auth_dict = json.load(json_file)
+        with open(os.path.join(json_folder, 'logon_map.json')) as json_file:
+            self.logon_dict = json.load(json_file)
+        with open(os.path.join(json_folder, 'orient_map.json')) as json_file:
+            self.orient_dict = json.load(json_file)
+        with open(os.path.join(json_folder, 'success_map.json')) as json_file:
+            self.success_dict = json.load(json_file)
+        with open(os.path.join(json_folder, 'other_map.json')) as json_file:
+            self.other_inds = json.load(json_file)
+        self.sos = 0
+        self.eos = 1
+        self.usr_OOV = 2
+        self.pc_OOV = 3
+        self.domain_OOV = 4
+        self.curr_ind = 5
 
     def run_detokenizer(self, tokens):	
         restored_lst = [self.search_dict[str(t)] for t in tokens]	
