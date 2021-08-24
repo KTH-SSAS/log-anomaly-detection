e55925d4739cf9fd453f47df1f1bcbed8ac13b65
Simon Gökstorp <gokstorp@kth.se>
2021-08-24 13:27:51 +0200

    Implement TransformerConfig + example config file

--- log_analyzer/config/model_config.py
+++ log_analyzer/config/model_config.py
@@ -30,3 +30,14 @@ class TieredLSTMConfig(LSTMConfig):
     def input_dim(self):
         """Feature length of input to LSTM"""
         return self.embedding_dim + self.context_layers[-1]
+
+class TransformerConfig(Config):
+    """Configuration class for Transformer models"""
+
+    def __init__(self, layers, attention_dim, attention_heads, embedding_dim, vocab_size):
+        super().__init__()
+        self.layers = layers
+        self.attention_dim = attention_dim
+        self.attention_heads = attention_heads
+        self.embedding_dim = embedding_dim
+        self.vocab_size = vocab_size
