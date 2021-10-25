01096b9d67720796e461f48fcffbce62d1755c3c
Simon Gökstorp <gokstorp@kth.se>
2021-10-25 13:02:00 +0200

    Make eval_model usable without wandb

--- eval_model.py
+++ eval_model.py
@@ -1,40 +1,51 @@
-import wandb
 import matplotlib.pyplot as plt
+import wandb
+from log_analyzer.application import Application
+from log_analyzer.trainer import Trainer
 
 
-def eval_model(model_trainer):
+def eval_model(model_trainer: Trainer):
     r"""Performs standard evaluation on the model. Assumes the model has been trained
     and the model's evaluator has been populated
     """
+    use_wandb = Application.instance().wandb_initialized
     model_trainer.evaluator.prepare_evaluation_data()
     # Get generic metrics
     evaluator_metrics = model_trainer.evaluator.get_metrics()
 
     # get line losses plot
     model_trainer.evaluator.plot_line_loss_percentiles(percentiles=[75,95,99], smoothing=300, ylim=(-1,-1), outliers=1, legend=False)
-    wandb.log({"Aggregate line losses": wandb.Image(plt)})
+    if use_wandb:
+        wandb.log({"Aggregate line losses": wandb.Image(plt)})
     plt.clf()
 
     # get roc curve
-    _, roc_plot = model_trainer.evaluator.plot_roc_curve(use_wandb=True)
-    wandb.log({"ROC Curve": roc_plot})
+    _, roc_plot = model_trainer.evaluator.plot_roc_curve(use_wandb=use_wandb)
+    if use_wandb:
+        wandb.log({"ROC Curve": roc_plot})
+
+    # get pr curve
+    AP_score, pr_plot = model_trainer.evaluator.plot_pr_curve(use_wandb=use_wandb)
+    if use_wandb:
+        wandb.log({"PR Curve": pr_plot})
+    evaluator_metrics["eval/AP"] = AP_score
+
 
     # Normalise the data
     model_trainer.evaluator.normalize_losses()
 
     # get normalised line losses plot
     model_trainer.evaluator.plot_line_loss_percentiles(percentiles=[75,95,99], smoothing=300, ylim=(-1,-1), outliers=1, legend=False)
-    wandb.log({"Aggregate line losses (normalised)": wandb.Image(plt)})
+    if use_wandb:
+        wandb.log({"Aggregate line losses (normalised)": wandb.Image(plt)})
     plt.clf()
 
     # get normalised roc curve
-    evaluator_metrics["eval/AUC_(normalised)"], roc_plot = model_trainer.evaluator.plot_roc_curve(title="ROC (normalised)", use_wandb=True)
-    wandb.log({"ROC Curve (normalised)": roc_plot})
+    evaluator_metrics["eval/AUC_(normalised)"], roc_plot = model_trainer.evaluator.plot_roc_curve(title="ROC (normalised)", use_wandb=use_wandb)
+    if use_wandb:
+        wandb.log({"ROC Curve (normalised)": roc_plot})
 
     # Log the evaluation results
-    #table_columns = [key for key in evaluator_metrics.keys()]
-    #table_data = [evaluator_metrics[key] for key in table_columns]
-    #metrics_table = wandb.Table(columns=table_columns, data=table_data)
-    #wandb.log({"Evaluation metrics": metrics_table})
-    for key in evaluator_metrics.keys():
-        wandb.run.summary[key] = evaluator_metrics[key]
+    if use_wandb:
+        for key in evaluator_metrics.keys():
+            wandb.run.summary[key] = evaluator_metrics[key]
