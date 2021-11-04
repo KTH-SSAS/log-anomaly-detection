63cf1de96c4f92d09c39cbdcbceaabc562532043
Yeongwoo Kim <yeongwoo@kth.se>
2021-11-04 14:28:06 +0100

    debugging for train loader and test loader

--- log_analyzer/train_loop.py
+++ log_analyzer/train_loop.py
@@ -103,7 +103,7 @@ def init_from_config_classes(model_type, bidirectional, model_config: LSTMConfig
                                                                 max_input_length, num_steps=3,
                                                                 context_layers=model_config.context_layers)
         lm_trainer = TieredTrainer(
-            trainer_config, model_config, bidirectional, log_dir, train_loader)
+            trainer_config, model_config, bidirectional, log_dir, train_loader, test_loader)
     elif model_type == LSTM:
         train_loader, test_loader = data_utils.load_data(data_folder, train_days, test_days,
                                                          trainer_config.batch_size, bidirectional, skip_sos, jagged,
