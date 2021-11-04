8f895fa3ed4b6c968e4a906f09f9b22bc6224cb7
Yeongwoo Kim <yeongwoo@kth.se>
2021-11-04 14:25:05 +0100

    debugging for train_loader and test_loader

--- log_analyzer/tiered_trainer.py
+++ log_analyzer/tiered_trainer.py
@@ -74,7 +74,7 @@ class TieredTrainer(Trainer):
                 output, ctxt_vector, ctxt_hidden, ctxt_cell = self.model(
                     X, ctxt_vector, ctxt_hidden, ctxt_cell, lengths=L
                 )
-                self.data_handler.update_state(ctxt_vector, ctxt_hidden, ctxt_cell)
+                self.train_loader.update_state(ctxt_vector, ctxt_hidden, ctxt_cell)
 
                 # Compute the loss for the output
                 loss, *_ = self.compute_loss(output, Y, lengths=L, mask=M)
@@ -83,7 +83,7 @@ class TieredTrainer(Trainer):
             output, ctxt_vector, ctxt_hidden, ctxt_cell = self.model(
                 X, ctxt_vector, ctxt_hidden, ctxt_cell, lengths=L
             )
-            self.data_handler.update_state(ctxt_vector, ctxt_hidden, ctxt_cell)
+            self.train_loader.update_state(ctxt_vector, ctxt_hidden, ctxt_cell)
 
             # Compute the loss for the output
             loss, *_ = self.compute_loss(output, Y, lengths=L, mask=M)
