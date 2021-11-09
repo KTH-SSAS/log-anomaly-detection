eebe8efecb91f4043adfcc9521ff3a7d7fccc110
Jakob Nyberg <jaknyb@kth.se>
2021-11-09 12:45:02 +0100

    parametrize tests

--- test/test_attention.py
+++ test/test_attention.py
@@ -6,13 +6,13 @@ import utils
 from log_analyzer.train_loop import init_from_config_classes, train_model
 
 
-def test_forward_char_attention(tmpdir):
-    bidir = False
-    model_type = "lstm"
-    token_level = "char"
+@pytest.mark.parametrize("tokenization", ["word", "char"])
+@pytest.mark.parametrize("bidir", [True, False])
+@pytest.mark.parametrize("attention_type", ["fixed", "semantic"])
+def test_attention(tmpdir, tokenization, bidir, attention_type):
 
-    args = utils.set_args(bidir, model_type, token_level)
-    args["model_config"].attention_type = "fixed"
+    args = utils.set_args(bidir, "lstm", tokenization)
+    args["model_config"].attention_type = attention_type
     args["model_config"].attention_dim = 10
     args["base_logdir"] = os.path.join(tmpdir, "runs")
 
@@ -20,99 +20,21 @@ def test_forward_char_attention(tmpdir):
     assert True
 
 
-def test_forward_word_attention(tmpdir):
+@pytest.mark.parametrize(
+    "tokenization,bidir",
+    [
+        ("word", True),
+        ("word", False),
+        pytest.param("char", False, marks=pytest.mark.xfail),
+        pytest.param("char", True, marks=pytest.mark.xfail),
+    ],
+)
+def test_syntax_attention(tmpdir, tokenization, bidir):
     bidir = False
     model_type = "lstm"
-    token_level = "word"
 
-    args = utils.set_args(bidir, model_type, token_level)
-    args["model_config"].attention_type = "fixed"
-    args["model_config"].attention_dim = 10
-    args["base_logdir"] = os.path.join(tmpdir, "runs")
-
-    utils.run_test(args)
-    assert True
-
-
-def test_forward_char_syntax_attention(tmpdir):
-    bidir = False
-    model_type = "lstm"
-    token_level = "char"
-
-    args = utils.set_args(bidir, model_type, token_level)
+    args = utils.set_args(bidir, model_type, tokenization)
     args["model_config"].attention_type = "syntax"
     args["model_config"].attention_dim = 10
     args["base_logdir"] = os.path.join(tmpdir, "runs")
-    with pytest.raises(RuntimeError) as execinfo:
-        utils.run_test(args)
-    assert "sequence length has to bet set" in str(execinfo.value)
-    assert True
-
-
-def test_forward_word_syntax_attention(tmpdir):
-    bidir = False
-    model_type = "lstm"
-    token_level = "word"
-
-    args = utils.set_args(bidir, model_type, token_level)
-    args["model_config"].attention_type = "syntax"
-    args["model_config"].attention_dim = 10
-    args["base_logdir"] = os.path.join(tmpdir, "runs")
-
-    utils.run_test(args)
-    assert True
-
-
-def test_forward_char_semantic_attention(tmpdir):
-    bidir = False
-    model_type = "lstm"
-    token_level = "char"
-
-    args = utils.set_args(bidir, model_type, token_level)
-    args["model_config"].attention_type = "semantic"
-    args["model_config"].attention_dim = 10
-    args["base_logdir"] = os.path.join(tmpdir, "runs")
-    utils.run_test(args)
-    assert True
-
-
-def test_forward_word_semantic_attention(tmpdir):
-    bidir = False
-    model_type = "lstm"
-    token_level = "word"
-
-    args = utils.set_args(bidir, model_type, token_level)
-    args["model_config"].attention_type = "semantic"
-    args["model_config"].attention_dim = 10
-    args["base_logdir"] = os.path.join(tmpdir, "runs")
-
-    utils.run_test(args)
-    assert True
-
-
-def test_bidirectional_word_attention(tmpdir):
-    bidir = True
-    model_type = "lstm"
-    token_level = "word"
-
-    args = utils.set_args(bidir, model_type, token_level)
-    args["model_config"].attention_type = "fixed"
-    args["model_config"].attention_dim = 10
-    args["base_logdir"] = os.path.join(tmpdir, "runs")
-
-    utils.run_test(args)
-    assert True
-
-
-def test_bidirectional_char_attention(tmpdir):
-    bidir = True
-    model_type = "lstm"
-    token_level = "char"
-
-    args = utils.set_args(bidir, model_type, token_level)
-    args["model_config"].attention_type = "fixed"
-    args["model_config"].attention_dim = 10
-    args["base_logdir"] = os.path.join(tmpdir, "runs")
-
-    utils.run_test(args)
     assert True
--- test/test_models.py
+++ test/test_models.py
@@ -2,109 +2,34 @@
 import os
 
 import pytest
+import torch.cuda
 import utils
 
 
-def test_forward_word(tmpdir):
-    bidir = False
-    model_type = "lstm"
-    token_level = "word"
-    args = utils.set_args(bidir, model_type, token_level)
-    args["base_logdir"] = os.path.join(tmpdir, "runs")
-
-    train_losses, test_losses = utils.run_test(args)
-    assert True
 
 
-def test_forward_char(tmpdir):
-    bidir = False
-    model_type = "lstm"
-    token_level = "char"
+@pytest.mark.parametrize("tokenization", ["word", "char"])
+@pytest.mark.parametrize("bidir", [True, False])
+@pytest.mark.parametrize("model_type", ["lstm", "tiered-lstm"])
+@pytest.mark.parametrize("cuda", [True, False])
+def test_lstm(tmpdir, model_type, bidir, tokenization, cuda):
 
-    args = utils.set_args(bidir, model_type, token_level)
+    if cuda and not torch.cuda.is_available():
+        pytest.skip()
+
+    args = utils.set_args(bidir, model_type, tokenization)
     args["base_logdir"] = os.path.join(tmpdir, "runs")
 
     utils.run_test(args)
     assert True
 
 
-def test_bidirectional_word(tmpdir):
-    bidir = True
-    model_type = "lstm"
-    token_level = "word"
+# TODO make the char test not fail
+@pytest.mark.parametrize("tokenization", ["word", pytest.param("char", marks=pytest.mark.skip(reason="Not implemented"))])
+def test_transformer(tmpdir, tokenization):
 
-    args = utils.set_args(bidir, model_type, token_level)
+    args = utils.set_args(False, "transformer", tokenization)
     args["base_logdir"] = os.path.join(tmpdir, "runs")
 
-    utils.run_test(args)
-    assert True
-
-
-def test_bidirectional_char(tmpdir):
-    bidir = True
-    model_type = "lstm"
-    token_level = "char"
-
-    args = utils.set_args(bidir, model_type, token_level)
-    args["base_logdir"] = os.path.join(tmpdir, "runs")
-
-    utils.run_test(args)
-    assert True
-
-
-def test_tiered_char(tmpdir):
-    bidir = False
-    model_type = "tiered-lstm"
-    token_level = "char"
-
-    args = utils.set_args(bidir, model_type, token_level)
-    args["base_logdir"] = os.path.join(tmpdir, "runs")
-
-    utils.run_test(args)
-    assert True
-
-
-def test_tiered_word(tmpdir):
-    bidir = False
-    model_type = "tiered-lstm"
-    token_level = "word"
-
-    args = utils.set_args(bidir, model_type, token_level)
-    args["base_logdir"] = os.path.join(tmpdir, "runs")
-
-    utils.run_test(args)
-    assert True
-
-
-def test_tiered_bidirectional_char(tmpdir):
-    bidir = True
-    model_type = "tiered-lstm"
-    token_level = "char"
-
-    args = utils.set_args(bidir, model_type, token_level)
-    args["base_logdir"] = os.path.join(tmpdir, "runs")
-
-    utils.run_test(args)
-    assert True
-
-
-def test_tiered_bidirectional_word(tmpdir):
-    bidir = True
-    model_type = "tiered-lstm"
-    token_level = "word"
-
-    args = utils.set_args(bidir, model_type, token_level)
-    args["base_logdir"] = os.path.join(tmpdir, "runs")
-
-    utils.run_test(args)
-    assert True
-
-
-def test_transformer_word(tmpdir):
-    bidir = False
-    model_type = "transformer"
-    token_level = "word"
-    args = utils.set_args(bidir, model_type, token_level)
-    args["base_logdir"] = os.path.join(tmpdir, "runs")
     utils.run_test(args)
     assert True
