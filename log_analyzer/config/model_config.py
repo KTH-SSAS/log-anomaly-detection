63a4b910a71710e42ef3049af965fa4179eef5c3
Yeongwoo Kim <yeongwoo@kth.se>
2021-11-12 16:15:22 +0100

    config classes with dataclass

--- log_analyzer/config/model_config.py
+++ log_analyzer/config/model_config.py
@@ -36,32 +36,28 @@ class TieredLSTMConfig(LSTMConfig):
         """Feature length of input to LSTM."""
         return self.embedding_dim + self.context_layers[-1]
 
-class TransformerConfig(Config):
-    """Configuration class for Transformer models"""
+@dataclass
+class TransformerConfig(ModelConfig):
+    """Configuration class for Transformer models."""
 
-    def __init__(self, layers, feedforward_dim, model_dim, attention_heads, vocab_size, dropout, sequence_length=None):
-        super().__init__()
-        self.layers = layers
-        self.feedforward_dim = feedforward_dim
-        self.model_dim = model_dim
-        self.attention_heads = attention_heads
-        self.vocab_size = vocab_size
-        self.dropout = dropout
-        self.sequence_length = sequence_length
+    layers: int
+    feedforward_dim: int
+    model_dim: int
+    attention_heads: int
+    vocab_size: int
+    dropout: float
 
+@dataclass
 class TieredTransformerConfig(TransformerConfig):
-    """Configuration class for Tiered Transformer models"""
-
-    def __init__(self, context_model_dim, context_layers, context_feedforward_dim, context_attention_heads, context_dropout, shift_window, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.context_model_dim = context_model_dim
-        self.context_layers =  context_layers
-        self.context_feedforward_dim = context_feedforward_dim
-        self.context_attention_heads = context_attention_heads
-        self.context_dropout = context_dropout
-        self.shift_window = shift_window
+    """Configuration class for Tiered Transformer models."""
+    context_model_dim: int
+    context_layers: int
+    context_feedforward_dim: int
+    context_attention_heads: int
+    context_dropout: float
+    shift_window: int
 
     @property
     def input_dim(self):
-        """Feature length of input to LSTM"""
+        """Feature length of input to LSTM."""
         return self.model_dim + self.context_model_dim
