145116c6956d63d90aca507e2840166bc1e6c7d9
Jakob Nyberg <jaknyb@kth.se>
2021-11-16 09:33:28 +0100

    standardize config names

--- test/utils.py
+++ test/utils.py
@@ -11,25 +11,17 @@ def set_args(bidir, model_type, token_level):
 
     args["bidirectional"] = bidir
     args["model_type"] = model_type
-    trainer_config = TrainerConfig.init_from_file("config/config_trainer.json")
+    trainer_config = TrainerConfig.init_from_file("config/lanl_config_trainer.json")
     trainer_config.train_files = ["0.txt", "1.txt"]
     trainer_config.test_files = ["2.txt"]
     args["trainer_config"] = trainer_config
-
-    if model_type == "tiered-lstm":
-        model_config_file = f"config/lanl_{token_level}_config_model_tiered.json"
-
-    elif model_type == "transformer":
-        # TODO We should standardize the config file names
-        model_config_file = f"config/lanl_{token_level}_config_{model_type}.json"
-
-    else:
-        model_config_file = f"config/lanl_{token_level}_config_model.json"
+    
+    model_config_file = f"config/lanl_config_{model_type}_{token_level}.json"
 
     args["model_config"] = get_model_config(model_config_file, model_type)
 
     args["data_folder"] = f"data/test_data/{token_level}_day_split"
-    args["data_config"] = DataConfig.init_from_file(f"config/lanl_{token_level}_config_data.json")
+    args["data_config"] = DataConfig.init_from_file(f"config/lanl_config_data_{token_level}.json")
 
     # Return the prepared args
     return args
