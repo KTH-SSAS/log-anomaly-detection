9b9f2fd86febb3a232abda1f67e0023a4253c30b
Yeongwoo Kim <yeongwoo@kth.se>
2022-01-24 18:14:55 +0100

    parameters for low level transformer

--- test/test_tiered_transformer.py
+++ test/test_tiered_transformer.py
@@ -3,3 +3,13 @@ import torch
 
 from log_analyzer.config.model_config import TieredTransformerConfig, TransformerConfig
 from log_analyzer.model.transformer import TieredTransformer
+
+CONSECUTIVE_LOG = 3
+SEQUENCE_LENGTH = 10
+VOCAB_SIZE = 128
+BATCH_SIZE = 64
+SHIFT_WINDOW = 10
+LOW_LV_MODEL_DIM = 64
+LOW_LV_FFW_DIM = 64
+DROPOUT_RATE = 0.1
+ATTENTION_HEAD = 2
