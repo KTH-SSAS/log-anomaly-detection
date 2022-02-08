1db2ca85cfd64ce49705aea85c77119da11dae20
Yeongwoo Kim <yeongwoo@kth.se>
2022-02-08 21:35:10 +0100

    check validity of the data handler.

--- test/test_tiered_data_loader.py
+++ test/test_tiered_data_loader.py
@@ -4,6 +4,10 @@ import torch
 from log_analyzer.config.trainer_config import DataConfig
 from log_analyzer.data.data_loader import TieredTransformerBatcher
 
+def batch_equal(v1: torch.Tensor, v2: torch.Tensor):
+    assert v1.shape == v2.shape
+    return all(torch.all((v1 == v2), dim=-1))
+
 @pytest.mark.parametrize("shuffle", [False, True])
 @pytest.mark.parametrize("bidirectional", [False, True])
 def test_tiered_data_loader_word(shuffle, bidirectional):
@@ -36,3 +40,8 @@ def test_tiered_data_loader_word(shuffle, bidirectional):
 
     for batch in data_handler:
         x: torch.Tensor = batch["input"]
+        assert x.shape == torch.Size([batch_size, input_length]), "bidirectional" if bidirectional else "forward"
+        assert batch_equal(
+            batch["input"][:, 1 : batch["input"].shape[1] - int(bidirectional)],
+            batch["target"][:, : batch["target"].shape[1] - int(not bidirectional)],
+        ), f"{'bidir' if bidirectional else 'forward'}-shift"  # Confirm that the targets are equal to the inputs shifted by 1
