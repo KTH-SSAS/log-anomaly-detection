f8fdb651eb2e20af4316a1cb441c10e4ed291c24
Yeongwoo Kim <yeongwoo@kth.se>
2022-02-08 21:32:38 +0100

    parameters

--- test/test_tiered_data_loader.py
+++ test/test_tiered_data_loader.py
@@ -7,3 +7,14 @@ from log_analyzer.data.data_loader import TieredTransformerBatcher
 @pytest.mark.parametrize("shuffle", [False, True])
 @pytest.mark.parametrize("bidirectional", [False, True])
 def test_tiered_data_loader_word(shuffle, bidirectional):
+
+    filepath = "data/test_data/word_day_split/0.txt"
+    data_config = DataConfig.init_from_file("config/lanl_config_data_word.json")
+    batch_size = 10
+    skip_sos = False
+    jagged = False
+    model_dim = 128
+    context_model_dim = 128
+    context_input_dimension = 128
+    shift_window = 100
+    num_steps = 3
