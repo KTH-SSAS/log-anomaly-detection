fbdd5681b9dc94d2b88fa42f462aaa3daac6aeee
Yeongwoo Kim <yeongwoo@kth.se>
2022-02-08 21:31:39 +0100

    pytest parametrize handler

--- test/test_tiered_data_loader.py
+++ test/test_tiered_data_loader.py
@@ -4,3 +4,5 @@ import torch
 from log_analyzer.config.trainer_config import DataConfig
 from log_analyzer.data.data_loader import TieredTransformerBatcher
 
+@pytest.mark.parametrize("shuffle", [False, True])
+@pytest.mark.parametrize("bidirectional", [False, True])
