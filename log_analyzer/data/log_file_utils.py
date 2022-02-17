93b9765b32d89a95da18af7bd0f6a674db62b0e5
Jakob Nyberg <jaknyb@kth.se>
2022-02-17 09:15:03 +0100

    cli tools

--- log_analyzer/data/log_file_utils.py
+++ log_analyzer/data/log_file_utils.py
@@ -1,15 +1,16 @@
+import csv
 import json
 import os
 import tempfile
 from argparse import ArgumentParser
 from collections import OrderedDict
+from log_analyzer.tokenizer.tokenizer_neo import LANLVocab
 
 SECONDS_PER_DAY = 86400
-import csv
 
 
 class LANLReader:
-    def __init__(self, file_pointer, already_split=False, has_red=False) -> None:
+    def __init__(self, file_pointer, normalized=False, has_red=False) -> None:
         self.field_names = [
             "time",
             "src_user",
@@ -34,7 +35,7 @@ class LANLReader:
             "auth_orient",
             "success",
         ]
-        self.already_split = already_split
+        self.normalized = normalized
         self.has_red = has_red
         self.file_pointer = file_pointer
 
@@ -42,15 +43,19 @@ class LANLReader:
             self._csv_field_names += "is_red"
             self.field_names += "is_red"
 
-        if already_split:
+        if normalized:
             self._csv_field_names = self.field_names
 
     def __iter__(self):
         reader = csv.DictReader(self.file_pointer, fieldnames=self._csv_field_names)
         for row in reader:
+
+            if row[self.field_names[-1]] is None:
+                raise RuntimeError(f"The number of fields in the data does not match the settings provided.")
+
             data = row
 
-            if not self.already_split:
+            if not self.normalized:
                 for u, d in zip(["src_user", "dst_user"], ["src_domain", "dst_domain"]):
                     data[u], data[d] = data[f"{u}@{d}"].split("@")
                     del data[f"{u}@{d}"]
@@ -79,7 +84,7 @@ def split_by_day(log_filename, out_dir, keep_days=None):
         os.mkdir(out_dir)
 
     current_day = 0
-    get_filename = lambda day: os.path.join(out_dir, f"{day}.csv")
+    def get_filename(day): return os.path.join(out_dir, f"{day}.csv")
     out_file = None
     with open(log_filename) as f:
         reader = LANLReader(f)
@@ -172,39 +177,75 @@ def process_logfiles_for_training(auth_file, red_file, output_dir, days_to_keep)
             add_redteam_to_log(infile, outfile, red_file)
 
 
-def count_fields(infile_path, outfile_path=None, fields_to_exclude=None):
+def count_fields(infile_path, outfile_path=None, fields_to_exclude=None, normalized=False, has_red=False):
+
     counts = OrderedDict()
-    with open(infile_path) as f:
-        reader = LANLReader(f)
 
-        fields = reader.field_names
-        for f in fields_to_exclude:
-            del fields[f]
+    if isinstance(infile_path, str):
+        infile_path = [infile_path]
 
-        for field in fields:
-            counts[field] = {}
+    for file in infile_path:
+        with open(file) as f:
+            reader = LANLReader(f, normalized=normalized, has_red=False)
 
-        for line in reader:
-            for k in fields:
-                v = line[k]
-                try:
-                    counts[k][v] += 1
-                except KeyError:
-                    counts[k][v] = 1
+            fields = reader.field_names.copy()
+            for f in fields_to_exclude:
+                del fields[f]
 
-    if outfile_path is not None:
-        with open(outfile_path, "w") as f:
-            json.dump(counts, f)
+            for field in fields:
+                counts[field] = {}
+
+            for line in reader:
+                for k in fields:
+                    v = line[k]
+                    try:
+                        counts[k][v] += 1
+                    except KeyError:
+                        counts[k][v] = 1
+
+        if outfile_path is not None:
+            with open(outfile_path, "w") as f:
+                json.dump(counts, f)
 
     return counts
 
 
 def process_file():
     parser = ArgumentParser()
-    parser.add_argument("--auth-file", type=str, help="Path to auth.txt.")
-    parser.add_argument("--redteam-file", type=str, help="Path to file with redteam events.")
-    parser.add_argument("-o", "--output", type=str, help="Output directory.")
-    parser.add_argument("days-to-keep", nargs="+", type=int, help="Days to keep logs from.")
+    parser.add_argument("auth_file", type=str, help="Path to auth.txt.")
+    parser.add_argument("redteam_file", type=str, help="Path to file with redteam events.")
+    parser.add_argument("days_to_keep", nargs="+", type=int, help="Days to keep logs from.")
+    parser.add_argument("-o", "--output", type=str, help="Output directory.", default="auth_split")
+    args = parser.parse_args()
+    process_logfiles_for_training(args.auth_file, args.redteam_file, args.output, args.days_to_keep)
+
+
+def generate_counts():
+    parser = ArgumentParser()
+    parser.add_argument("log_files", type=str, nargs="+", help="Path(s) to log file(s).")
+    parser.add_argument("--not-normalized", action="store_false",
+                        help="Add this flag if the log file is not already normalized.")
+    parser.add_argument("--no-red", action="store_false",
+                        help="Add this flag if the log file does not have red team events added.")
+    parser.add_argument("-o", "--output", help="Output filename.", default="counts.json")
+    parser.add_argument("--fields-to-exclude", nargs="+", type=int, help="Indexes of fields to not count.", default=[])
+    args = parser.parse_args()
+    #args = parser.parse_args(["data/auth_by_day/8.csv", "data/auth_by_day/7.csv", "--fields-to-exclude", "0", "--no-red", "--not-normalized"])
+    count_fields(args.log_files, args.output, args.fields_to_exclude, args.not_normalized, args.no_red)
+
+
+def generate_vocab_from_counts():
+
+    parser = ArgumentParser()
+    parser.add_argument("counts_file", type=str, help="Path to JSON file with field counts.")
+    parser.add_argument("--mode", choices=["fields", "global"])
+    parser.add_argument("--cutoff", type=int,
+                        help="If a token occurs less than the cutoff value, it will not be included.")
+    parser.add_argument("-o", "--output", type=str, help="Output filename", default="vocab.json")
+
     args = parser.parse_args()
 
-    process_logfiles_for_training(args.auth_file, args.redteam_file, args.output, args.days_to_keep)
+    if args.mode == "fields":
+        LANLVocab.counts2vocab(args.counts_file, args.output, args.cutoff)
+    elif args.mode == "global":
+        raise NotImplementedError("Globally unique vocab is not implemented.")
