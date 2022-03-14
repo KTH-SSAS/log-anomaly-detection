1f9d25bdf81a298813282f4e116ec744be3c1171
Jakob Nyberg <jaknyb@kth.se>
2022-03-14 09:34:29 +0000

    add short argument names

---
+++ train.sh
@@ -0,0 +1,2 @@
+ 
+n train_model.py --model-type transformer --model-config config/lanl_config_transformer_char.json --data-config config/lanl_config_data_char.json --data-folder lanl/ --trainer-config config/lanl_config_trainer.json --wandb_sync --use-cuda --eval_model 
