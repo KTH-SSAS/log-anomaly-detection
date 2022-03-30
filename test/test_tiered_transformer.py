0cd6495b6968fd1eb09779ea51863940f8e6fe73
Yeongwoo Kim <yeongwoo@kth.se>
2022-03-30 09:49:01 +0200

    adding device argument to get_ctx_data.

--- test/test_tiered_transformer.py
+++ test/test_tiered_transformer.py
@@ -16,11 +16,17 @@ def test_tiered_transformer_forward_word(
 
     tieredTransformer = TieredTransformer(test_tiered_transformer_config, bidirectional=False)
     ctx_lengths_before_run = pad_sequence(
-        tieredTransformer.get_ctx_data(torch.squeeze(test_tiered_transformer_input[0]))[0], batch_first=True
+        tieredTransformer.get_ctx_data(
+            torch.squeeze(test_tiered_transformer_input[0]), test_tiered_transformer_input[1].device
+        )[0],
+        batch_first=True,
     ).shape[0]
     token_output, _ = tieredTransformer(test_tiered_transformer_input, context_history)
     ctx_lengths_after_run = pad_sequence(
-        tieredTransformer.get_ctx_data(torch.squeeze(test_tiered_transformer_input[0]))[0], batch_first=True
+        tieredTransformer.get_ctx_data(
+            torch.squeeze(test_tiered_transformer_input[0]), test_tiered_transformer_input[1].device
+        )[0],
+        batch_first=True,
     ).shape[0]
     assert min(ctx_lengths_before_run + consecutive_log, shift_window) == ctx_lengths_after_run
     assert token_output.shape == torch.Size([consecutive_log, batch_size, sequence_length, vocab_size])
