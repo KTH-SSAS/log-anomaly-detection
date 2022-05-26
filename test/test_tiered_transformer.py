3a55839d68a9d0e19bac653225033b14c9c33101
Yeongwoo Kim <yeongwoo@kth.se>
2022-05-26 16:49:40 +0200

    quick fix

--- test/test_tiered_transformer.py
+++ test/test_tiered_transformer.py
@@ -16,17 +16,13 @@ def test_tiered_transformer_forward_word(
 
     tieredTransformer = TieredTransformer(test_tiered_transformer_config, bidirectional=False)
     ctx_lengths_before_run = pad_sequence(
-        tieredTransformer.get_ctx_data(
-            torch.squeeze(test_tiered_transformer_input[0]), test_tiered_transformer_input[1].device
-        )[0],
+        tieredTransformer.get_ctx_data(test_tiered_transformer_input[0], test_tiered_transformer_input[1].device),
         batch_first=True,
-    ).shape[0]
+    ).shape[1]
     token_output, _ = tieredTransformer(test_tiered_transformer_input, context_history)
     ctx_lengths_after_run = pad_sequence(
-        tieredTransformer.get_ctx_data(
-            torch.squeeze(test_tiered_transformer_input[0]), test_tiered_transformer_input[1].device
-        )[0],
+        tieredTransformer.get_ctx_data(test_tiered_transformer_input[0], test_tiered_transformer_input[1].device),
         batch_first=True,
-    ).shape[0]
+    ).shape[1]
     assert min(ctx_lengths_before_run + consecutive_log, shift_window) == ctx_lengths_after_run
     assert token_output.shape == torch.Size([consecutive_log, batch_size, sequence_length, vocab_size])
