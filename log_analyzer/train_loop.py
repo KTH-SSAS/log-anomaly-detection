14ce324385f207b4e4ccc5b7df82aa0db18bae61
Simon Gökstorp <gokstorp@kth.se>
2022-05-30 14:38:14 +0200

    Linting and toxing

--- log_analyzer/train_loop.py
+++ log_analyzer/train_loop.py
@@ -354,7 +354,7 @@ def train_model(lm_trainer: Trainer, train_loader, val_loader):
                             "train/day": batch["day"][0],
                             "train/lr": lm_trainer.optimizer.param_groups[0]["lr"],
                             "train/epoch": epoch,
-                            "train/gradient_norm": gradient_norm
+                            "train/gradient_norm": gradient_norm,
                         },
                     )
                     if run_validation and epoch_iteration > 0 and (epoch_iteration % validation_period == 0):
